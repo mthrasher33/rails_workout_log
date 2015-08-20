d6d25b3b54a0f9b7696631c7cc1395a18ffa7c45
Matt Thrasher <Matt@Upwards-MacBook-Pro.local>
2015-08-20 14:37:53 -0500

    Add exercises to workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,57 +1,48 @@
 class WorkoutsController < ApplicationController
 	before_action :find_workout, only: [:show, :edit, :update, :destroy]
-
-def index
-	@workouts = Workout.all.order("created_at DESC")
-end
-
-def show
-	@workout = Workout.find(params[:id])
-end
-
-
-def new
-	@workout = Workout.new
-end
-
-def create
-	@workout = Workout.new(workout_params)
-	if @workout.save
-		redirect_to @workout
-	else
-		render "New"
-	end
-end
-
-def edit
-	@workout = Workout.find(params[:id])
-end
- 
-
-
-def update
-	if @workout.update(workout_params)
-		redirect_to @workout
-	else
-		render "Edit"
+	def index
+		@workouts = Workout.all.order("created_at DESC")
 	end
 
-end
+	def show
+	end
 
-def destroy
-	@workout.destroy
-	redirect_to root_path
-end
+	def new
+		@workout = Workout.new
+	end
 
-private
+	def create
+		@workout = Workout.new(workout_params)
+		if @workout.save
+			redirect_to @workout
+		else
+			render 'new'
+		end
+	end
 
-def workout_params
-	params.require(:workout).permit(:workout, :date, :mood, :length)
-end
+	def edit
+	end
 
-def find_workout
-	@workout = Workout.find(params[:id])
-end
+	def update
+		if @workout.update(workout_params)
+			redirect_to @workout
+		else
+			render 'edit'
+		end
+	end
 
+	def destroy
+		@workout.destroy
+		redirect_to root_path
+	end
 
+	private
+
+	def workout_params
+		params.require(:workout).permit(:date, :workout, :mood, :length)
+	end
+
+	def find_workout
+		@workout = Workout.find(params[:id])
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820014158) do
+ActiveRecord::Schema.define(version: 20150820144805) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "sets"
+    t.integer  "reps"
+    t.integer  "workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "exercises", ["workout_id"], name: "index_exercises_on_workout_id"
 
   create_table "models", force: :cascade do |t|
     t.string   "workout"
